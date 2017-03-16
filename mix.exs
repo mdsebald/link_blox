eb64e7f1a384096c8874b8ea9cc57477f1c1d1fe
Mark Sebald <mdsebald@users.noreply.github.com>
2017-03-16 15:01:54 -0500

    Update to match Nerves version 0.5.0

--- mix.exs
+++ mix.exs
@@ -1,45 +1,81 @@
 defmodule NervesLinkBlox.Mixfile do
   use Mix.Project
 
-  @target System.get_env("NERVES_TARGET") || "rpi3"
+  @target System.get_env("MIX_TARGET") || "host"
+
+  Mix.shell.info([:green, """
+  Env
+    MIX_TARGET:   #{@target}
+    MIX_ENV:      #{Mix.env}
+  """, :reset])
 
   def project do
     [app: :nerves_linkblox,
-     version: "0.0.1",
+     version: "0.1.0",
+     elixir: "~> 1.4.0",
      target: @target,
      archives: [nerves_bootstrap: "~> 0.3.0"],
      deps_path: "deps/#{@target}",
      build_path: "_build/#{@target}",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     aliases: aliases(),
-     deps: deps() ++ system(@target)]
+     aliases: aliases(@target),
+     deps: deps()]
   end
 
   # Configuration for the OTP application.
   #
   # Type `mix help compile.app` for more information.
-  def application do
-    [mod: {NervesLinkBlox, []},
-     applications: [:logger, 
-                    :nerves_start_network,
-                    :nerves,
-                    :nerves_system_br,  
-                    :erlang_ale,
-                    :LinkBlox]]
+  def application, do application(@target)
+
+  # Specify target specific application configurations
+  # It is common that the application start function will start and supervise
+  # applications which could cause the host to fail. Because of this, we only
+  # invoke NervesLinkBlox.start/2 when running on a target.
+  def application("host") do
+    [extra_applications: [:logger, 
+                          :nerves_start_network,
+                          :nerves,
+                          :nerves_system_br,  
+                          :erlang_ale,
+                          :LinkBlox]]
+  end
+  def application(_target) do
+   [mod: {NervesLinkBlox, []},
+     extra_applications: [:logger, 
+                          :nerves_start_network,
+                          :nerves,
+                          :nerves_system_br,  
+                          :erlang_ale,
+                          :LinkBlox]]
   end
 
+  # Dependencies can be Hex packages:
+  #
+  #   {:my_dep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+  #
+  # Type "mix help deps" for more examples and options
   def deps do
-    [{:nerves, "~> 0.5.0"},
+    [{:nerves, "~> 0.5.0"}, runtime: false] ++
+    deps(@target)
+  end
+ 
+  # Specify target specific dependencies
+  def deps("host"), do: []
+  def deps(target) do
+    [{:nerves_runtime, "~> 0.1.0"},
+     {:"nerves_system_#{target}", "~> 0.11.0", runtime: false},
      {:nerves_start_network, github: "mdsebald/nerves_start_network"},
      {:LinkBlox, github: "mdsebald/LinkBlox"}]
   end
 
-  def system(target) do
-    [{:"nerves_system_#{target}", "~> 0.11.0"}]
-  end
-
-  def aliases do
+  # We do not invoke the Nerves Env when running on the Host
+  def aliases("host"), do: []
+  def aliases(_target) do
     ["deps.precompile": ["nerves.precompile", "deps.precompile"],
      "deps.loadpaths":  ["deps.loadpaths", "nerves.loadpaths"]]
   end
